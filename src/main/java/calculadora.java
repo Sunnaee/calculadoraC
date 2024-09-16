fe8aa2e6e469fce81c548107732d1a118d31fc38
emiunu <e.jaramillo02@ufromail.cl>
2024-09-16 14:44:56 -0300

    Primera version de la actividad 4

--- src/main/java/calculadora.java
+++ src/main/java/calculadora.java
@@ -1,83 +1,52 @@
-import java.util.Arrays;
-
-import static java.lang.Math.sqrt;
 
 public class calculadora {
     public static void main(String[] args) {
-        double [] numeros = new double[3];
-        numeros[0] = 2;
-        numeros[1] = 3;
-        numeros[2] = 5;
-        System.out.println(solucionCuadratica1(numeros));
-        System.out.println(solucionCuadratica2(numeros));
+        double [][] ecuaciones = new double[2][3];
+        ecuaciones[0] = new double[]{1,2,1};
+        ecuaciones[1] = new double[]{1,1,2};
+        resultados(ecuaciones);
 
     }
 
-    public static double mayorDeNumeros(double[] numeros) {
-        return Arrays.stream(numeros).max().getAsDouble();
+    //SISTEMAS DE ECUACIONES
+
+    public static double determinante(double[][] ecuaciones){
+        double a1 = ecuaciones[0][0];
+        double b1 = ecuaciones[0][1];
+        double a2 = ecuaciones[1][0];
+        double b2 = ecuaciones[1][1];
+        return (a1*b2)-(a2*b1);
     }
 
-    public static double menorDeNumeros(double[] numeros) {
-        return Arrays.stream(numeros).min().getAsDouble();
+    public static double determinanteX(double[][] ecuaciones){
+        double b1 = ecuaciones[0][1];
+        double c1 = ecuaciones[0][2];
+        double b2 = ecuaciones[1][1];
+        double c2 = ecuaciones[1][2];
+        return (c1*b2)-(c2*b1);
     }
 
-    public static boolean numerosIguales(double [] numeros){
-        return numeros[0] == numeros[1];
+    public static double determinanteY(double[][] ecuaciones){
+        double a1 = ecuaciones[0][0];
+        double c1 = ecuaciones[0][2];
+        double a2 = ecuaciones[1][0];
+        double c2 = ecuaciones[1][2];
+        return (a1*c2)-(a2*c1);
     }
 
-    public static String numeroMayor(double[] numeros){
-        if (numerosIguales(numeros)) {
-            return "Numeros iguales";
-        }else{
-            return "El número mayor es: " + mayorDeNumeros(numeros);
-        }
+    public static double resultadoX(double[][] ecuaciones){
+        return (determinanteX(ecuaciones))/(determinante(ecuaciones));
     }
 
-    public static String numeroMenor(double[] numeros){
-        if (numerosIguales(numeros)) {
-            return "Numeros iguales";
-        } else {
-            return "El número menor es: " + menorDeNumeros(numeros);
-        }
+    public static double resultadoY(double[][] ecuaciones){
+        return (determinanteY(ecuaciones))/(determinante(ecuaciones));
     }
 
-    public static void ejecutarNumeroMayor(double[] numeros){
-        System.out.println(numeroMayor(numeros));
+    public static void resultados(double[][] ecuaciones){
+        System.out.println("Soluciones:");
+        System.out.println("x = " + resultadoX(ecuaciones));
+        System.out.println("y = " + resultadoY(ecuaciones));
     }
-
-    public static void ejecutarNumeroMenor(double[] numeros){
-        System.out.println(numeroMenor(numeros));
-    }
-
-    public static double potenciaNumeros(double [] numeros){
-        return Math.pow(numeros[0], numeros[1]);
-    }
-
-    public static boolean ceroSobreCero(double[] numeros){
-        return numeros[0] == 0 && numeros[1] == 0;
-    }
-
-    public static String resultadoPotencia(double[] numeros){
-        if(ceroSobreCero(numeros)){
-            return "No se puede elevar cero sobre cero";
-        }
-        return "El resultado es: " + potenciaNumeros(numeros);
-    }
-
-    public static double porcentajeNumero(double[] numeros){
-        return (numeros[0]/100) * numeros[1]; //numero[0] es el porcentaje que quiero calcular.
-    }
-
-    //Este entrega el error NaN, que es not a number cuando la solucion es imaginaria
-    public static double solucionCuadratica1(double[] numeros){
-        return (-(numeros[1]) + sqrt((numeros[1]*numeros[1])-(4*numeros[0]*numeros[2]))) / (2*numeros[0]);
-    }
-
-    //Este entrega el error NaN, que es not a number cuando la solucion es imaginaria
-    public static double solucionCuadratica2(double[] numeros){
-        return (-(numeros[1]) - sqrt((numeros[1]*numeros[1])-(4*numeros[0]*numeros[2]))) / (2*numeros[0]);
-    }
-
 
 }
 
