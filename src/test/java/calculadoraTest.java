24feab3cf3c2f09929233296b68bbc44c9ff23e0
Sabrina <s.lopez15@ufromail.cl>
2024-09-16 17:13:21 -0300

    Se realizan las pruebas unitarias de la 2da versión del código (versión con manejo del error de dividir por cero).

--- src/test/java/calculadoraTest.java
+++ src/test/java/calculadoraTest.java
@@ -6,11 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class calculadoraTest {
 
     double[][] matrizPuntos = new double[2][2];
+    double[][] matrizPuntos2 = new double[2][2];
+
     @BeforeEach
     void setUp() {
         System.out.println("Preparándo matriz recta para una prueba...");
         matrizPuntos[0] = new double[]{-5,-2};
         matrizPuntos[1] = new double[]{5,4};
+        matrizPuntos2[0] = new double[]{7,-6};
+        matrizPuntos2[1] = new double[]{7,1};
     }
 
     @AfterEach
@@ -18,11 +22,19 @@ class calculadoraTest {
         System.out.println("Limpiando matriz recta para una prueba...");
         matrizPuntos[0] = new double[]{0,0};
         matrizPuntos[1] = new double[]{0,0};
+        matrizPuntos2[0] = new double[]{0,0};
+        matrizPuntos2[1] = new double[]{0,0};
     }
 
     @Test
     void pendienteRecta() {
         assertEquals(0.6,calculadora.pendienteRecta(matrizPuntos));
+    }
+
+    @Test
+    void rectaParalelaEjeY() {
+        assertFalse(calculadora.rectaParalelaEjeY(matrizPuntos));
+        assertTrue(calculadora.rectaParalelaEjeY(matrizPuntos2));
     }
 
     @Test
